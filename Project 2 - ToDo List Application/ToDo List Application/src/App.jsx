c1d9bdf2a429b753daa0f1fba373020a017b81db
Clara <clarapachecodev@gmail.com>
2023-05-13 09:06:51 -0300

    Work on the functionalities of the application by creating state variables and events for input and button + displaying tasks inside of the bottom list

--- Project 2 - ToDo List Application/ToDo List Application/src/App.jsx
+++ Project 2 - ToDo List Application/ToDo List Application/src/App.jsx
@@ -1,17 +1,44 @@
+import { useState } from "react"
+
 import "./App.css"
 
-
 export function App() {
+
+  const [todoList, settodoList] = useState([])
+  const [currentTask, setCurrentTask] = useState("")
+
+
   return(
     <div className="app"> 
 
       <h1>ToDo List </h1>
 
       <div className="task">
-        <input type="text" placeholder="Enter Task" />
-        <button>Add Task</button>
+        <input
+          type="text"
+          placeholder="Enter Task"
+          onChange={(event) => {
+            setCurrentTask(event.target.value)
+          }}
+        />
+        <button
+          onClick={() => {
+            settodoList([...todoList, currentTask])
+          }}
+        >
+          Add Task
+        </button>
       </div>
       <hr />
+      <div className="list">
+        {
+          todoList.map((task)=>{
+            return(
+              <li key={task}>{task}</li>
+            )
+          })
+        }
+      </div>
     
     </div>
   )
