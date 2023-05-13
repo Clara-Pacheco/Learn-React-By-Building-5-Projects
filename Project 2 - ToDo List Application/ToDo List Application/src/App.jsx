bb692de9f097ade12c88a3cdede64b6c4760d218
Clara <clarapachecodev@gmail.com>
2023-05-13 08:42:35 -0300

    Build html structure

--- Project 2 - ToDo List Application/ToDo List Application/src/App.jsx
+++ Project 2 - ToDo List Application/ToDo List Application/src/App.jsx
@@ -1,4 +1,20 @@
+import "./App.css"
+
+
 export function App() {
+  return(
+    <div className="app"> 
+
+      <h1>ToDo List </h1>
+
+      <div className="task">
+        <input type="text" placeholder="Enter Task" />
+        <button>Add Task</button>
+      </div>
+      <hr />
+    
+    </div>
+  )
   
 }
 
