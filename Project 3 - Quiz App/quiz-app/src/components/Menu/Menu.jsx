b3ac9711df24c6e38577f7c33db45a2271762e26
Clara <clarapachecodev@gmail.com>
2023-05-14 09:50:06 -0300

    Add label and class 'menu' to Menu component

--- Project 3 - Quiz App/quiz-app/src/components/Menu/Menu.jsx
+++ Project 3 - Quiz App/quiz-app/src/components/Menu/Menu.jsx
@@ -2,7 +2,8 @@ import styles from './Menu.module.css'
 
 export function Menu() {
   return (
-    <div className={styles.startq}>
+    <div className={styles.menu}>
+      <label>Enter your name: </label>
       <input type="text" placeholder='Ex. John Smith' />
       <button>Start Quiz</button>
     </div>
