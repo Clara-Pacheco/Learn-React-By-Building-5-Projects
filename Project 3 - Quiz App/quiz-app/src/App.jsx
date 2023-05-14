e5f651cf81fd26fbdcb5989b0db9d8bf985af115
Clara <clarapachecodev@gmail.com>
2023-05-14 09:48:36 -0300

    Import useState from react and create a gameState that is initialized by the menu component

--- Project 3 - Quiz App/quiz-app/src/App.jsx
+++ Project 3 - Quiz App/quiz-app/src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
+
 import styles from "./App.module.css"
 
 import { Menu } from './components/Menu/Menu'
 
 export function App() {
+  const [gameState, setGameState] = useState("menu")
+
   return (
     <div className={styles.app}>
       <h1>Quiz App</h1>
-      <Menu />
+      {gameState === "menu" && <Menu />}
+      
     </div>
   )
 }
