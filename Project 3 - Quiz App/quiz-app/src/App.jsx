48dc60a65463c79ab45cef9ae9f177b3a8db86cb
Clara <clarapachecodev@gmail.com>
2023-05-14 08:54:42 -0300

    Create a module css file for App component

--- Project 3 - Quiz App/quiz-app/src/App.jsx
+++ Project 3 - Quiz App/quiz-app/src/App.jsx
@@ -1,8 +1,13 @@
-import "./style.css"
+import styles from "./App.module.css"
+
+import { Menu } from './components/Menu/Menu'
 
 export function App() {
   return (
-    <div className="app">Hello</div>
+    <div className={styles.app}>
+      <h1>Quiz App</h1>
+      <Menu />
+    </div>
   )
 }
 
